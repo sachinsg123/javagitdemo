7b04800cec84a5c28ef7b7458ba261e7038bd224
sachin <sachinghule834@gmail.com>
2024-06-28 12:25:19 +0530

    commit by developer2

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition{
+
+
+    public void add(){
+
+        System.out.println("this is addition method");
+    }
+}
---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+public class Multiplication {
+
+
+    public void mul(){
+
+        System.out.println("created by developer 2");
+    }
+    
+}
